0fa2dc47574ef33c1f9fe21556dd22d131f07d03
lucianodamasceno <luciano.galdinosilva@hotmail.com>
2021-09-04 00:12:10 -0300

    fim do modulo

--- Type-number.js
+++ Type-number.js
@@ -1,2 +1,21 @@
-const meuNumero = 3;
-console.log(meuNumero);
+// = tipo number
+
+//const meuNumero = 3;
+//console.log(meuNumero)
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log(operacaoMatematica);
+// = ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+//console.log(novaOperacao);
+
+
+// NaN -> Not a Number 
+const alura = "Alura";
+console.log(alura * primeiroNumero)
