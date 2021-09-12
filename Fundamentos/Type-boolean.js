27e57d88c901d5762e511137cc766f05a6e503cc
lucianodamasceno <luciano.galdinosilva@hotmail.com>
2021-09-12 19:09:46 -0300

    correcao

--- Fundamentos/Type-boolean.js
+++
@@ -1,8 +0,0 @@
-const primeiroNumero = 5;
-const segundoNumero = 10;
-//console.log(primeiroNumero == segundoNumero);
-let cadastroAtivado = true;
-
-const texto1 = "Alura";
-const texto2 = "a";
-console.log(texto1 === texto2)
--- Fundamentos/expressoes-funcao.js
+++
@@ -1,2 +0,0 @@
-const soma = function(num1, num2){return num1 + num2};
-console.log(soma(1,1));
